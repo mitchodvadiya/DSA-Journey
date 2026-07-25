a783cbfd57f1a1a89b35b1cf254d9da1adeac8fe
Mit Chodvadiya <mitchodavadiya@gmail.com>
2026-07-25 22:40:58 +0530

    Time: 3 ms (38.40%) | Memory: 10 MB (85.32%) - LeetSync

---
+++ 968-beautiful-array/beautiful-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> beautifulArray(int n) {
+        vector<int> ans;
+        ans.push_back(1);
+
+        while (ans.size() < n) {
+            vector<int> temp;
+
+            for (int i = 0; i < ans.size(); i++) {
+                if (2 * ans[i] - 1 <= n) {
+                    temp.push_back(2 * ans[i] - 1);
+                }
+            }
+
+            for (int i = 0; i < ans.size(); i++) {
+                if (2 * ans[i] <= n) {
+                    temp.push_back(2 * ans[i]);
+                }
+            }
+            ans = temp;
+        }
+        return ans;
+    }
+};
